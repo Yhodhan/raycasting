075c0c1f08fcb229839b51989d463f39ef819ecf
Yhodhan <mscuderi855@gmail.com>
2024-07-23 23:03:28 -0300

    Initial setup

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+rm -rf build
+mkdir -p build
+cmake -B build
---
+++ build/CMakeFiles/raycasting.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/raycasting.dir/src/glad.c.o"
+  "CMakeFiles/raycasting.dir/src/glad.c.o.d"
+  "CMakeFiles/raycasting.dir/src/main.cpp.o"
+  "CMakeFiles/raycasting.dir/src/main.cpp.o.d"
+  "raycasting"
+  "raycasting.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/raycasting.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
